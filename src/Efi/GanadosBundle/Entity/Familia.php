f8b09ed5eb4544397afd04ce3bd10128c4422e47
Daniel Futrillé <futridan@gmail.com>
2016-10-17 22:30:57 -0400

    Creación Entity Persona

---
+++ src/Efi/GanadosBundle/Entity/Familia.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Efi\GanadosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Efi\GeneralBundle\Entity\Estado;
+use Efi\GeneralBundle\Entity\Municipio;
+
+/**
+ * Familia
+ *
+ * @ORM\Table(name="familias", indexes={@ORM\Index(name="fk_FAMILIAS_ESTADOS1_idx", columns={"EDO_ID"}), @ORM\Index(name="fk_FAMILIAS_MUNICIPIOS1_idx", columns={"MCP_ID"})})
+ * @ORM\Entity
+ */
+class Familia
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="FAM_ID", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="FAM_NOMBRE", type="string", length=50, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="FAM_DIRECCION", type="string", length=500, nullable=false)
+     */
+    private $direccion;
+
+    /**
+     * @var Estado
+     *
+     * @ORM\ManyToOne(targetEntity="Estado")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="EDO_ID", referencedColumnName="EDO_ID")
+     * })
+     */
+    private $estado;
+
+    /**
+     * @var Municipio
+     *
+     * @ORM\ManyToOne(targetEntity="Municipio")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="MCP_ID", referencedColumnName="MCP_ID")
+     * })
+     */
+    private $municipio;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @param string $direccion
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+    }
+
+    /**
+     * @return Estado
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param Estado $estado
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
+
+    /**
+     * @return Municipio
+     */
+    public function getMunicipio()
+    {
+        return $this->municipio;
+    }
+
+    /**
+     * @param Municipio $municipio
+     */
+    public function setMunicipio($municipio)
+    {
+        $this->municipio = $municipio;
+    }
+
+
+}
